3c75883142b2c877acd21de135e1e08bfa93c799
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-10-20 22:42:40 -0300

    feature: crud para gerenciamento de planos

---
+++ backend/src/app/controllers/PlanController.js
@@ -0,0 +1,88 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlansController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const plans = await Plan.findAll({
+      attributes: ['id', 'title', 'duration', 'price'],
+      limit: 20,
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      price: Yup.number()
+        .required()
+        .positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha de validação!' });
+    }
+
+    const planExists = await Plan.findOne({
+      where: { title: req.body.title },
+    });
+
+    if (planExists) {
+      return res.status(400).json({ error: 'Este plano já existe!' });
+    }
+
+    const { id, title, duration, price } = await Plan.create(req.body);
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      price: Yup.number()
+        .required()
+        .positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha de validação!' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+
+    const { id, title, duration, price } = await plan.update(req.body);
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
+
+  async delete(req, res) {
+    const plan = await Plan.findByPk(req.params.id);
+
+    plan.destroy();
+
+    return res.json();
+  }
+}
+
+export default new PlansController();
